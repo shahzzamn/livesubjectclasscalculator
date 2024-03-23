515790b9adca9aec3c708f4502d57a016f197fa3
shahzaman <161032970+shahzzamn@users.noreply.github.com>
2024-03-24 04:46:02 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+import inquirer from "inquirer";
+const asnwer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter second number", type: "number", name: "secondNumber" },
+    { message: "Select one of the oprators to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multipilication", "Division"],
+    },
+]);
+console.log(asnwer);
+//conditinal Statments
+if (asnwer.operator === "Addition") {
+    console.log(asnwer.firstNumber + asnwer.secondNumber);
+}
+else if (asnwer.operator === "Subtraction") {
+    console.log(asnwer.firstNumber - asnwer.secondNumber);
+}
+else if (asnwer.operator === "Multipilication") {
+    console.log(asnwer.firstNumber * asnwer.secondNumber);
+}
+else if (asnwer.operator === "Division") {
+    console.log(asnwer.firstNumber / asnwer.secondNumber);
+}
